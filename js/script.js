acdc21f5b38739aded505fb09a6d3d4348d3a8b0
KTerka <katarzynapawela@gmail.com>
2017-07-05 19:31:11 +0200

    Application is ready - guote on tweeter

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 $(function(){
-	
+
+var prefix = "https://cors-anywhere.herokuapp.com/";	
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
+    $.getJSON(prefix+quoteUrl, createTweet);
+	$.ajaxSetup({ cache: false });
 }
 
 function createTweet(input) {
@@ -18,7 +20,7 @@ function createTweet(input) {
     }
 	
 	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-	if (tweetText.lenght > 140) {
+	if (tweetText.length > 140) {
 		getQuote();
 	}else  {
 		var tweet = tweetLink + encodeURIComponent(tweetText);
@@ -28,6 +30,13 @@ function createTweet(input) {
 	}
 		
 }
+	
+$(document).ready(function() {
+    getQuote();
+    $('.trigger').click(function() {
+        getQuote();
+    })
+});
 	
 });
 
